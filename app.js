b909241899ebf312482e5c37471826e07b36b48c
Karthik Sunil K <karthiksunil038@gmail.com>
2020-12-14 23:05:25 +0530

    image name console

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var upload = require('express-fileupload');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(upload())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
+var upload = require('express-fileupload');
 
+var app = express();
+app.use(upload)
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.post('/submit',function(req,res){
-  console.log(req.body)
   MongoClient.connect("mongodb://localhost:27017",function(err,client){
     if(err)
       console.log('error')
@@ -16,7 +18,8 @@ router.post('/submit',function(req,res){
       client.db('clientdata').collection('user').insertOne(req.body)
   })
   res.render('success',{ name:req.body.first_name});
-  console.log(req.body.first_name)
+  console.log(req.body)
+  console.log(req.files.photo)
 })
 
 module.exports = router;
